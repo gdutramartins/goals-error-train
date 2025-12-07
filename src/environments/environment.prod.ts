61181debb6fedb14fc4d371cc426f2401c2a2a05
gdutramartins <gdutramartins@gmail.com>
2025-12-07 09:34:18 -0300

    environment

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyAOMra3zH_6n3JI1wS4603JjCdrI5ir7yU',
+    authDomain: 'goals-error-train.firebaseapp.com',
+    projectId: 'goals-error-train',
+    storageBucket: 'goals-error-train.firebasestorage.app',
+    messagingSenderId: '1060426621134',
+    appId: '1:1060426621134:web:3eef43e6ab8313eb93309f',
+    measurementId: 'G-RN3H6REGS2',
+  },
+};
